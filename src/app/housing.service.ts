ec0d5d6564098065b487044e94dc70e763c9d5f7
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2025-04-23 17:56:47 +0900

    formの追加

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import {HousingLocation } from './housing-location';
+import { HousingLocation } from './housing-location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
   protected housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -115,5 +118,11 @@ export class HousingService {
 
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+        `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
   }
 }
