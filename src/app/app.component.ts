8be2557378a1c30e7dbc9db73eeebf9fddd00d0e
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2025-04-23 14:34:39 +0900

    homeのroutingを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
-import {HomeComponent} from './home/home.component';
-import {Router, RouterModule} from "@angular/router";
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-root',
