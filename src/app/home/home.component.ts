1d3a059639684d89914aaac3108faabb572bc721
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2025-04-23 14:25:28 +0900

    routeの大枠のみを追加

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocationComponent } from '../housing-location/housing-location.component';
 import {HousingLocation } from '../housing-location';
+import {HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -22,4 +23,9 @@ import {HousingLocation } from '../housing-location';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+    constructor() {
+        this.housingLocationList = this.housingService.getAllHousingLocations();
+    }
 }
